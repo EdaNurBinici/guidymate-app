6aa94663ba772a9435797c3fe37273722c7de490
edanurbinici <edanur0x0@gmail.com>
2026-02-14 01:53:41 +0300

    Fix vite config

--- APP/frontend/vite.config.js
+++ APP/frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 5173
   }
